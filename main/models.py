36885c016b960080757272ed3bbd71b1af1a7ae4
aminardisy <ardi.dadih27@gmail.com>
2024-09-07 23:45:16 +0700

    Penyelesaian tugas 2 hingga local host

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Gunpla(models.Model):
+    name =  models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    size_ratio = models.CharField(max_length=10)
+    extensions = models.CharField(max_length=255)
+    notes = models.TextField()
+
+# Create your models here.
---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'name' : 'MG GUNDAM F90',
+        'price': 'RP 420.000,00 ',
+        'size ratio': '1/100',
+        'extensions': 'stand base',
+        'notes':'*The product in the image is a prototype still in its developmental stage. The product is also painted. The actual product may appear differently from the image. *Please note that in some cases bubbles may enter the clear parts during manufacturing process.'
+    }
+
+    return render(request, "main.html", context)
+# Create your views here.
