a5703a5dbe62cda6c7d1cc64782842624984364d
aminardisy <ardi.dadih27@gmail.com>
2024-09-09 23:58:37 +0700

    migrate models

--- main/models.py
+++ main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Gunpla(models.Model):
     name =  models.CharField(max_length=255)
-    price = models.IntegerField(max_length=255)
-    description = models.TextField()
+    price = models.IntegerField()
+    description = models.TextField(default='No description available')
     size_ratio = models.CharField(max_length=10)
     extensions = models.CharField(max_length=255)
     notes = models.TextField()
