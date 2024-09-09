444af7724c7bae10b72063ce637a0a57ac6cf9cc
aminardisy <ardi.dadih27@gmail.com>
2024-09-09 17:56:35 +0700

    koreksi views.py

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ def show_main(request):
     context = {
         'name' : 'MG GUNDAM F90',
         'price': 'RP 420.000,00 ',
-        'size ratio': '1/100',
+        'size_ratio': '1/100',
         'extensions': 'stand base',
         'notes':'*The product in the image is a prototype still in its developmental stage. The product is also painted. The actual product may appear differently from the image. *Please note that in some cases bubbles may enter the clear parts during manufacturing process.'
     }
